a8cd3e6f3c606387fd25181dd22341d746d9cdf0
Marinho <hmarinho.dev@gmail.com>
2025-12-16 16:30:46 -0300

    add nuxt/ui and tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: true,
-  modules: ['@nuxt/eslint', '@pinia/nuxt'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@pinia/nuxt',
+  ],
+  css: ['~/assets/css/main.css'],
 });
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="p-2">Dashboard Analytics</h1>
+</template>
